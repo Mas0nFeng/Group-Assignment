655a221a22ec1a381ce64d6253f91ac5c0d69e3b
Mason Feng <masonf9988@outlook.com>
2023-10-24 16:56:45 +1100

    Code

--- GroupAssignment.m
+++ GroupAssignment.m
@@ -13,7 +13,8 @@ sig2 = zeros(Niter,1);
 sig2(1) = var(y);
 
 
-theta0 = [0.4347 0.5128 0.02677]; % Starting value. Using a standard normal random number for simplicity
+theta0 = [0.4347 0.5128 0.02677];
+theta0 = randn(1,dim);% Starting value. Using a standard normal random number for simplicity
 markov_chain(1,:) = theta0; % Just setting starting value as the first value in the markov chain
 n = 1;
 
@@ -29,16 +30,16 @@ while n<Niter
         alpha = (exp(t2)*exp(t3))/(1+exp(t2)+exp(t3)+exp(t2)*exp(t3));
         beta = (exp(t2))/(1+exp(t2)+exp(t3)+exp(t2)*exp(t3));
 
-        sig2(n) = omega + markov_chain(n-1,2)*y(n-1)^2 + markov_chain(n-1,3)*sig2(n-1);
+        sig2(n) = omega + alpha*y(n-1)^2 + beta*sig2(n-1);
     else
         ;
     end
     
-    Sigma = eye(dim);
+    Sigma = eye(dim)*0.001;
 
     epsilon = mvnrnd(zeros(dim,1),Sigma); % Drawing from proposal dist
     proposal = markov_chain(n,:)+epsilon; % Adding epsilon to get the new proposal
-    auxiliary = kernel(proposal,y,sig2(n))-kernel(markov_chain(n,:),y,sig2(n)); % Computing our diff in log-likelihoods for proposal and previous value
+    auxiliary = kernel(proposal,y(n),sig2(n))-kernel(markov_chain(n,:),y(n),sig2(n)); % Computing our diff in log-likelihoods for proposal and previous value
     alpha = min(exp(auxiliary),1); % Computing acceptance prob. Remembering to exp the auxillary as it was in log-scale
     u = rand; % Generate U from uniform. We will use to accept/reject our new proposa
     % Simple if-else, for accepting/rejecting our proposal. If
@@ -57,15 +58,15 @@ hold on
 
 subplot(3,1,1)
 plot(markov_chain(:,1))
-title('\beta_0')
+title('\theta_1')
 
 subplot(3,1,2)
 plot(markov_chain(:,2))
-title('\beta_1')
+title('\theta_2')
 
 subplot(3,1,3)
 plot(markov_chain(:,3))
-title('\beta_2')
+title('\theta_3')
 
 %%
 
@@ -87,14 +88,8 @@ log_prior = @(alpha, beta) log(gamma_constant*(alpha^0.5)*((1-alpha)^9)) + log(g
 
 % Jacobian
 
-J = @(theta) [exp(theta(1)) 0 0; %row1
-    0 (exp(theta(3))*(exp(theta(2))+exp(theta(2)+theta(3))))/(1+exp(theta(2))+exp(theta(3))+exp(theta(2)+theta(3)))^2
-    (exp(theta(2))*(exp(theta(3))+exp(theta(2)+theta(3))))/(1+exp(theta(2))+exp(theta(3))+exp(theta(2)+theta(3)))^2; %row2
-    0 (exp(theta(2))+exp(theta(2)+theta(3)))/(1+exp(theta(2))+exp(theta(3))+exp(theta(2)+theta(3)))^2
-    -(exp(theta(2))*(exp(theta(3))+exp(theta(2)+theta(3))))/(1+exp(theta(2))+exp(theta(3))+exp(theta(2)+theta(3)))^2];%row3
-
 detJ = @(theta) -((exp(theta(1)+2*theta(2)+theta(3)))/((exp(theta(2)+1))^3)*(exp(theta(3)+1))^2);
 
-% Calculating the kernel: log prior + log likelihood
-llh = log_prior(theta(2), theta(3))+log_like(y, sig2);%+log(detJ(theta));
+% Calculating the kernel: log prior + log likelihood + log detJ
+llh = log_prior(theta(2), theta(3))+log_like(y, sig2)+log(detJ(theta));
 end
